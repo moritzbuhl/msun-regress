c17b839b048608e4a9aa98ba6d6ca575fb2795d1
mbuhl <git@moritzbuhl.de>
2021-10-22 18:32:39 +0200

    fixup copyright

--- atf-c.h
+++ atf-c.h
@@ -1,6 +1,6 @@
 /*	$OpenBSD$	*/
 /*
- * Copyright (c) 2019 - 2021 Moritz Buhl <mbuhl@openbsd.org>
+ * Copyright (c) 2019-2021 Moritz Buhl <mbuhl@openbsd.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
